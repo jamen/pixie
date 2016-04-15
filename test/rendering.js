43899518acaf4d78d8f386bfb32295452746dcc5
Jamen Marzonie <jamenmarz+gh@gmail.com>
2016-04-15 15:39:17 -0600

    Add rendering (#6)
    * Add engines and serialization
    
    * Add rendering

--- test/rendering.js
+++ test/rendering.js
@@ -4,16 +4,13 @@ var test = require('tape');
 var sem = require('../lib');
 
 test.skip('rendering', function(t) {
-  t.plan(1);
-
-  // Template
-  var source = '</foo/> bar </baz/> qux </foo/>';
   var data = {foo: 'hello', baz: 'world'};
 
-  // Rendering
   t.same(
-    sem.render(source, {data: data}),
+    sem.render('{{foo}} bar {{baz}} qux {{foo}}', data),
     'hello bar world qux hello',
     'correct output'
   );
+
+  t.end();
 });
