3e5478f70dce9ff2c4ed1eee4e71fa6942f66d84
Dustin Dowell <dustindowell22@gmail.com>
2016-04-14 23:04:40 -0500

    new format

--- test/parsing.js
+++ test/parsing.js
@@ -4,19 +4,11 @@ var test = require('tape');
 var sem = require('../lib');
 
 test('parsing', function(t) {
-  t.plan(1);
-
-  // Template
-  var source = '{{foo}} bar {{baz}} qux {{foo}}';
-
-  // String parsing
-  var tpl = sem.parse(source);
-  t.same(
-    tpl,
+  t.deepEqual(sem.parse('{{foo}} bar {{baz}} qux {{foo}}'),
     {
       partials: ['', ' bar ', ' qux ', ''],
       expressions: ['foo', 'baz', 'foo']
-    },
-    'correct output'
+    }
   );
+  t.end();
 });
