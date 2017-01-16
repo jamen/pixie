56683afa27c3a00b3c8fad2271b8d33c50585dc5
Dustin Dowell <dustindowell22@gmail.com>
2017-01-15 22:02:47 -0600

    Fix slicing alignment (#21)

--- index.js
+++ index.js
@@ -2,30 +2,47 @@ exports.parse = parse
 exports.compile = compile
 exports.render = render
 
+function pad(v){for(var r='';v--;)r+=' ';return r}
+
 function parse (source, open, close) {
-  open = open || '{{', close = close || '}}'
-  var index = 0, previous = null, max = source.length, osize = open.length, csize = close.length,
-  partials = [], expressions = [];
-  while (max > index) {
-    var opening = source.indexOf(open, index)
-    if (opening === -1) break
-    var previous = index
-    partials.push(source.slice(previous, opening))
-    opening += size
-    index = source.indexOf(close, opening)
-    expressions.push(source.slice(opening, index))
+  open = open || '{{'
+  close = close || '}}'
+
+  var openLength = open.length,
+      closeLength = close.length,
+      i = 0,
+      end = source.length,
+      partials = [],
+      expressions = [],
+      first,
+      source = pad(closeLength) + source // "hack" to avoid checking starting index each itteration
+
+  while (end > i) {
+    // get index of first open
+    first = source.indexOf(open, i)
+
+    // break if there's no first open
+    if (first === -1) break
+
+    // this commented out code is the check that is avoided with the "hack"
+    // partials.push(source.slice(i ? i + closeLength : 0, first))
+    partials.push(source.slice(i + closeLength, first))
+
+    // set index of close starting at first open
+    i = source.indexOf(close, first)
+    expressions.push(source.slice(first + openLength, i))
   }
-  partials.push(source.slice(index))
+
+  partials.push(source.slice(i + closeLength))
   return [partials, expressions]
 }
 
 var NODATA = {}
 function compile (template, data) {
   data = data || NODATA
-  var source = '', partials = template[0], expressions = template[1], index = 0,
-  max = expressions.length
-  while (index < max) source += partials[index] + (data[expressions[index++]] || '')
-  return source + partials[index]
+  var source = '', partials = template[0], expressions = template[1], i = 0, end = expressions.length
+  while (i < end) source += partials[i] + (data[expressions[i++]] || '')
+  return source + partials[i]
 }
 
 function render (source, data, open, close) {
