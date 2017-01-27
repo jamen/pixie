4660a7836686b13d232ef5322fa7af42ee30fc78
Dustin Dowell <dustindowell22@gmail.com>
2017-01-26 21:59:10 -0600

    Faster parsing implementation

--- index.js
+++ index.js
@@ -2,38 +2,28 @@ exports.parse = parse
 exports.compile = compile
 exports.render = render
 
-function pad(v){for(var r='';v--;)r+=' ';return r}
-
 function parse (source, open, close) {
   open = open || '{{'
   close = close || '}}'
 
   var openLength = open.length,
       closeLength = close.length,
-      i = 0,
-      end = source.length,
       partials = [],
       expressions = [],
       first,
-      source = pad(closeLength) + source // "hack" to avoid checking starting index each itteration
+      last
 
-  while (end > i) {
-    // get index of first open
-    first = source.indexOf(open, i)
-
-    // break if there's no first open
-    if (first === -1) break
-
-    // this commented out code is the check that is avoided with the "hack"
-    // partials.push(source.slice(i ? i + closeLength : 0, first))
-    partials.push(source.slice(i + closeLength, first))
-
-    // set index of close starting at first open
-    i = source.indexOf(close, first)
-    expressions.push(source.slice(first + openLength, i))
+  for (;;) {
+    first = source.indexOf(open)
+    if (first < 0) break
+    partials.push(source.slice(0, first))
+    last = source.indexOf(close, first)
+    expressions.push(source.slice(first + openLength, last))
+    source = source.slice(last + closeLength)
   }
 
-  partials.push(source.slice(i + closeLength))
+  partials.push(source)
+
   return [partials, expressions]
 }
 
