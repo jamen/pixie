95f4bfcc8b1e86aac6dd3d9f7d850d6ff60883be
Dustin Dowell <dustindowell22@gmail.com>
2017-01-26 21:59:51 -0600

    add a new test for outer braces

--- test/parsing.js
+++ test/parsing.js
@@ -1,17 +1,9 @@
-var test = require('tape');
-var pixie = require('..');
+var test = require('tape')
+var pixie = require('..')
 
-test('parsing', function(t) {
-  t.same(pixie.parse('{{foo}} bar {{baz}} qux {{foo}}'),
-    [['', ' bar ', ' qux ', ''], ['foo', 'baz', 'foo']],
-    'simple template'
-  );
-
-  t.deepEqual(
-    pixie.parse('foo bar baz qux foo'),
-    [['foo bar baz qux foo'], []],
-    'no expressions'
-  );
-
-  t.end();
-});
+test('parse', function (t) {
+  t.same(pixie.parse('foo{{bar}}baz{{qux}}qix'), [['foo', 'baz', 'qix',], ['bar', 'qux']], 'simple template inner')
+  t.same(pixie.parse('{{foo}}bar{{baz}}qux{{qix}}'), [['', 'bar', 'qux', ''], ['foo', 'baz', 'qix']], 'simple template outer')
+  t.deepEqual(pixie.parse('foo bar baz qux qix'), [['foo bar baz qux qix'], []], 'no expressions')
+  t.end()
+})
