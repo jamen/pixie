fa53de0666c59cf269c1ed15ce4ef5d66882a1ea
Dustin Dowell <dustindowell22@gmail.com>
2017-04-05 14:56:04 -0500

    organize bench

--- bench.js
+++ bench.js
@@ -3,20 +3,22 @@ var pixie = require('./')
 var compile = pixie.compile
 var render = pixie.render
 
+var sample = 'hello {{world}} foo {{bar}} baz {{qux}} qix'
+var template = pixie(sample)
+var data = { world: 'WORLD', bar: 'BAR', qux: 'QUX' }
+
 test('benchmark', function (t) {
   t.plan(3)
 
   t.benchmark('parse', function () {
-    pixie('hello {{world}} foo {{bar}} baz {{qux}} qix')
+    pixie(sample)
   })
 
-  var template = pixie('foo {{bar}} baz {{qux}} qix {{qaz}} buz')
-  var data = { bar: 'BAAAR', qux: 'QUUUX', qaz: 'QAAAZ' }
   t.benchmark('compile', function () {
     compile(template, data)
   })
 
   t.benchmark('render', function () {
-    render('foo {{bar}} baz {{qux}} qix {{qaz}} buz', data)
+    render(sample, data)
   })
 })
