6f9633d6c28233bbc3c88b968f7fc6abd5aeaee3
Dustin Dowell <dustindowell22@gmail.com>
2017-05-27 18:50:02 -0500

    update tests

--- test.js
+++ test.js
@@ -5,9 +5,9 @@ var compile = pixie.compile
 
 test('parse', function (t) {
   t.plan(5)
-  t.same(parse('foo{{bar}}baz{{qux}}qix'), [['foo', 'baz', 'qix'], ['bar', 'qux']], 'inner expressions')
-  t.same(parse('{{foo}}bar{{baz}}qux{{qix}}'), [['', 'bar', 'qux', ''], ['foo', 'baz', 'qix']], 'outer expressions')
-  t.same(parse('foo bar baz qux qix'), [['foo bar baz qux qix'], []], 'no expressions')
+  t.same(parse('foo{{bar}}baz{{qux}}qix', '{{', '}}'), [['foo', 'baz', 'qix'], ['bar', 'qux']], 'inner expressions')
+  t.same(parse('{{foo}}bar{{baz}}qux{{qix}}', '{{', '}}'), [['', 'bar', 'qux', ''], ['foo', 'baz', 'qix']], 'outer expressions')
+  t.same(parse('foo bar baz qux qix', '{{', '}}'), [['foo bar baz qux qix'], []], 'no expressions')
   t.same(parse('foo <%bar%> baz <%qux%>', '<%', '%>'), [['foo ', ' baz ', ''], ['bar', 'qux']], 'custom tags')
   t.same(parse('foo __bar__ baz __qux__', '__', '__'), [['foo ', ' baz ', ''], ['bar', 'qux']], 'custom tags with same open and close')
 })
@@ -15,9 +15,9 @@ test('parse', function (t) {
 test('compile', function (t) {
   t.plan(4)
 
-  var sample1 = parse('foo{{bar}}baz{{qux}}qix')
-  var sample2 = parse('{{foo}}bar{{baz}}qux{{qix}}')
-  var sample3 = parse('foo bar baz qux qix')
+  var sample1 = parse('foo{{bar}}baz{{qux}}qix', '{{', '}}')
+  var sample2 = parse('{{foo}}bar{{baz}}qux{{qix}}', '{{', '}}')
+  var sample3 = parse('foo bar baz qux qix', '{{', '}}')
 
   t.is(compile(sample1, { bar: 'baaar', qux: 'quuux' }), 'foobaaarbazquuuxqix', 'inner expressions')
   t.is(compile(sample2, { foo: 'foooo', baz: 'baaaz', qix: 'qiiix' }), 'foooobarbaaazquxqiiix', 'outer expressions')
