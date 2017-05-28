43cd2d9dec3fba0ad4ce86d3eace7ac9f852fa0f
Jamen Marzonie <jamenmarz@gmail.com>
2017-05-27 20:10:39 -0500

    Change benchmarks to nanobench

--- bench.js
+++ bench.js
@@ -1,5 +1,6 @@
-var test = require('tape-benchmark')(require('tape'))
+var bench = require('nanobench')
 var pixie = require('./')
+
 var parse = pixie.parse
 var compile = pixie.compile
 
@@ -7,15 +8,20 @@ var sample = 'hello {{world}} foo {{bar}} baz {{qux}} qix'
 var template = parse(sample, '{{', '}}')
 var data = { world: 'WORLD', bar: 'BAR', qux: 'QUX' }
 
-test('benchmark', function (t) {
-  t.plan(3)
-  t.benchmark('parse', function () {
-    parse(sample, '{{', '}}')
-  })
-  t.benchmark('compile', function () {
-    compile(template, data)
-  })
-  t.benchmark('render', function () {
-    compile(parse(sample, '{{', '}}'), data)
-  })
+bench('pixie.parse', function (b) {
+  b.start()
+  parse(sample, '{{', '}}')
+  b.end()
+})
+
+bench('pixie.compile', function (b) {
+  b.start()
+  compile(template, data)
+  b.end()
+})
+
+bench('pixie.parse + pixie.compile (render)', function (b) {
+  b.start()
+  compile(parse(sample, '{{', '}}'), data)
+  b.end()
 })
